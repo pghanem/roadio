2e1ea484bbb3791db7b62a1d47de61c99f3326f4
Peter Ghanem <peterjonathang@gmail.com>
2025-01-20 13:09:04 -0800

    [fix] add type check to constants

--- config/env.ts
+++ config/env.ts
@@ -1,7 +1,11 @@
 import Constants from "expo-constants";
 
+if (!Constants.expoConfig?.extra?.SPOTIFY_CLIENT_ID) {
+    throw new Error("SPOTIFY_CLIENT_ID is not defined in app config");
+}
+
 export const SPOTIFY_CONFIG = {
-    CLIENT_ID: process.env.SPOTIFY_CLIENT_ID!,
-    CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET!,
-    REDIRECT_URI: process.env.SPOTIFY_REDIRECT_URI!,
+    CLIENT_ID: Constants.expoConfig?.extra?.SPOTIFY_CLIENT_ID ?? "",
+    CLIENT_SECRET: Constants.expoConfig?.extra?.SPOTIFY_CLIENT_SECRET ?? "",
+    REDIRECT_URI: Constants.expoConfig?.extra?.SPOTIFY_REDIRECT_URI ?? "",
 } as const;
